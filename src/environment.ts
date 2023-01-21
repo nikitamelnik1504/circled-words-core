2de1c3b6778a467ae78836681184c4b29b160a40
nikita <nikitamelnikwork@gmail.com>
2023-01-22 01:49:10 +0200

    Created env file. Disabled Apollo landing page in production environment.

---
+++ src/environment.ts
@@ -0,0 +1,17 @@
+const defaultPort = 4000;
+
+interface Environment {
+  env: string,
+  apollo: {
+    introspection: boolean,
+  },
+  port: number | string;
+}
+
+export const environment: Environment = {
+  env: <string>process.env.NODE_ENV,
+  apollo: {
+    introspection: process.env.APOLLO_INTROSPECTION === 'true',
+  },
+  port: process.env.PORT || defaultPort
+};
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import {ApolloServer} from "@apollo/server";
 import resolvers from './resolvers';
 import typeDefs from './type-defs';
 import {startStandaloneServer} from "@apollo/server/standalone";
+import {environment} from "./environment";
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+} from "@apollo/server/plugin/landingPage/default";
+
+import {ApolloServerPluginLandingPageDisabled} from "@apollo/server/plugin/disabled";
 
 (async () => {
-  const server = new ApolloServer({typeDefs, resolvers});
+  const server = new ApolloServer({
+    typeDefs, resolvers, introspection: environment.apollo.introspection, plugins: [
+      environment.env === 'production' ? ApolloServerPluginLandingPageDisabled
+      () : ApolloServerPluginLandingPageLocalDefault()
+    ]
+  });
   const {url} = await startStandaloneServer(server, {
-    listen: {port: 4000},
+    listen: {port: +environment.port},
   });
   console.log(`🚀  Server ready at: ${url}`);
 })();
