b5bd4237ee35a27eb77185ff5160db4fa94cf743
nikita <nikitamelnikwork@gmail.com>
2023-01-22 00:59:55 +0200

    Configured Apollo Server.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
+import {ApolloServer} from "@apollo/server";
+import resolvers from './resolvers';
+import typeDefs from './type-defs';
+import {startStandaloneServer} from "@apollo/server/standalone";
+
+(async () => {
+  const server = new ApolloServer({typeDefs, resolvers});
+  const {url} = await startStandaloneServer(server, {
+    listen: {port: 4000},
+  });
+  console.log(`🚀  Server ready at: ${url}`);
+})();
+
 // HMR
 if (module.hot) {
   module.hot.accept();
