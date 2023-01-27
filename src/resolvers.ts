568b0c26cb7dd6eb1e0101d83fed07f0f7912a9a
Nikita <nikitamelnikwork@gmail.com>
2023-01-27 15:37:30 +0200

    Added base resolver implementation.

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,11 +1,10 @@
-import {Circled, QueryResolvers, Resolvers} from "./generated/graphql";
+import {Resolvers} from "./generated/graphql";
+import CircledResolver from "./Resolvers/CircledResolver";
 
-export default <Resolvers>{
-    Query: <QueryResolvers>{
-        circled: (parent, args): Array<Circled> => {
-            return [
-                {tokenAddress: 'test'}
-            ];
-        }
+const resolvers: Resolvers = {
+    Query: {
+        circled: (parent, args) => ((new CircledResolver(<never>parent, args)).load())
     }
 }
+
+export default resolvers;
